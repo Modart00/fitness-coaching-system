f0a22693860661a1166b69a9b3803b0dfb08c86a
Modart00 <bkryldzz19@gmail.com>
2026-06-17 16:30:36 +0300

    add JWT service implementation

---
+++ src/main/java/com/modart00/fitness_coaching_system/security/JwtService.java
@@ -0,0 +1,72 @@
+package com.modart00.fitness_coaching_system.security;
+
+import com.modart00.fitness_coaching_system.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService{
+
+    private final String SECRET_KEY = "bW9kYXJ0MDBtb2RhcnQwMG1vZGFydDAwbW9kYXJ0MDBtb2RhcnQwMA==";
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(User user){
+        SecretKey secretKey = getSigningKey();
+
+        return Jwts.builder()
+                .subject(user
+                        .getEmail()).issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(secretKey)
+                .compact();
+
+    }
+
+    public Claims extractAllClaims(String token){
+        SecretKey secretKey = getSigningKey();
+
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String extractEmail(String token){
+        return extractAllClaims(token).getSubject();
+    }
+
+    public Date extractExpiration(String token){
+        return extractAllClaims(token).getExpiration();
+    }
+
+    public boolean isTokenExpired(String token){
+        Date currentDate = new Date(System.currentTimeMillis());
+        Date expirationDate = extractExpiration(token);
+
+        if (currentDate.after(expirationDate)){
+            return true; } else return false;
+
+    }
+
+    public boolean validateToken(String token,User user){
+        String email = extractEmail(token);
+        if (isTokenExpired(token) || !email.equals(user.getEmail())){
+            return false;
+        } else return true;
+    }
+
+}
